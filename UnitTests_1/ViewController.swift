0de7dc74a81191cf4ff8e2e62a4753f2d00496a9
DevBorisElkin <ea.foreverrr@gmail.com>
2022-08-07 19:12:04 +0300

    Initial commit

---
+++ UnitTests_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitTests_1
+//
+//  Created by test on 07.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
